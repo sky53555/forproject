53d6d52f32e47dba819dfcea26e8a2072299038a
hyung <13meal@gmail.com>
2018-06-01 16:40:15 +0900

    togglemenu

--- js/astyle.js
+++ js/astyle.js
@@ -1,3 +1,16 @@
+	// 상단 돋보기
+    $(".fa-search").click(function() {
+       $(".searchbox").toggle();
+       $("input[type='text']").val("");
+       $("input[type='text']").focus();
+    });
+
+	// header의 카테고리 선택
+	function goList(arg){
+		localStorage.aStyleItem=arg;
+		location.href="itemList.html";
+	};
+
 	// ===== Scroll to Top ==== 
 	$(window).scroll(function() {
 	    if ($(this).scrollTop() >= 150) {        // If page is scrolled more than 50px
