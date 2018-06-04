4578773584980c2dbfa7bd40570bd9375b08f5c3
hyung <13meal@gmail.com>
2018-06-04 10:29:00 +0900

    item

---
+++ js/item.js
@@ -0,0 +1,116 @@
+
+	var outerInfo=[
+		{title1:"Boy Crystal Blazer",		title2:"Filles a papa",				price:"350,000원"},
+		{title1:"Bailey Crystal Jacket",	title2:"Filles a papa",				price:"220,000원"},
+		{title1:"Vinyl Jacket",				title2:"3.1 Phillip Lim",			price:"390,000원"},
+		{title1:"Willow Coat",				title2:"Opportuno",					price:"430,000원"},
+		{title1:"Leather Lunchbag Clutch",	title2:"Filles a papa",				price:"510,000원"},
+		{title1:"Gaia's Ark Jade",			title2:"Opportuno",					price:"210,000원"},
+		{title1:"Gaia's Ark Tortoise",		title2:"Cult Gaia",					price:"210,000원"},
+		{title1:"Bond Bracelet",			title2:"Charlotte Chesnais",		price:"370,000원"},
+		{title1:"Caracol Earcuff",			title2:"Charlotte Chesnais",		price:"190,000원"},
+/*10*/	{title1:"Gaia's Hoops Large",		title2:"Cult Gaia",					price:"180,000원"},
+		{title1:"Wool Socks Camel",			title2:"CAP Studio",				price:"360,000원"},
+		{title1:"Logo Socks",				title2:"Golden Goose Deluxe Brand",	price:"280,000원"},
+		{title1:"Mirage Exotica Jacket",	title2:"Filles a papa",				price:"430,000원"},
+		{title1:"Bailey Crystal Jacke",		title2:"Filles a papa",				price:"350,000원"},
+		{title1:"Gaia's Ark Tortoise",		title2:"Golden Goose Deluxe",		price:"290,000원"},
+		{title1:"Leather Clutch",			title2:"Simon Miller",				price:"310,000원"},
+		{title1:"Bond Bracelet",			title2:"Charlotte Chesnais",		price:"250,000원"},
+		{title1:"Caracol Earcuff",			title2:"Cult Gaia",					price:"240,000원"} ];
+
+	var topInfo=[
+		{title1:"Striped Ruffle Shirt",		title2:"Xirena",					price:"110,000원"},
+		{title1:"Summer Tee",				title2:"Filles a papa",				price:"210,000원"},
+		{title1:"SPIRIPED SHIRT",			title2:"Xirena",					price:"230,000원"},
+		{title1:"Andrea Shirt",				title2:"Can Pep Rey",				price:"210,000원"},
+		{title1:"Oversized Velvet Tee",		title2:"Can pep rey",				price:"370,000원"},
+		{title1:"BONSAI BAG LARGE",			title2:"SIMON MILLER",				price: "350,000원"},
+		{title1:"Rainbow Sequin Pouch",		title2:"Attico",					price:"220,000원"},
+		{title1:"Golden Clutch",			title2:"Golden Goose Deluxe Brand",	price:"290,000원"},
+		{title1:"Caracol Earcuff",			title2:"Charlotte Chesnais",		price:"190,000원"},
+/*10*/	{title1:"Gaia's Hoops Large",		title2:"Cult Gaia",					price:"275,000원"},
+		{title1:"Wool Socks Camel",			title2:"CAP Studio",				price:"115,000원"},
+		{title1:"Logo Socks",				title2:"Paco Rabanne",				price:"180,000원"},
+		{title1:"Slava Shirt Voile",		title2:"Thierry Colson",			price:"230,000원"},
+		{title1:"Sandrine Top",				title2:"Ulla Johnson",				price:"150,000원"},
+		{title1:"Gaia's Ark Tortoise",		title2:"Golden Goose Deluxe",		price:"190,000원"},
+		{title1:"Puffed Sleeve Shirt",		title2:"3.1 Phillip Lim",			price:"310,000원"},
+		{title1:"Bond Bracelet",			title2:"Charlotte Chesnais",		price:"250,000원"},
+		{title1:"Caracol Earcuff",			title2:"Cult Gaia",					price:"140,000원"} ];
+
+	var bottomInfo=[
+		{title1:"Logo Socks",				title2:"Paco Rabanne",				price:"180,000원"},
+		{title1:"Bonsai Bag Mango",			title2:"Simon Miller",				price:"430,000원"},
+		{title1:"Gaia's Ark Jade",			title2:"Cult Gaia",					price:"150,000원"},
+		{title1:"Gaia's Ark Tortoise",		title2:"Golden Goose Deluxe",		price:"290,000원"},
+		{title1:"Leather Clutch",			title2:"Simon Miller",				price:"110,000원"},
+		{title1:"BONSAI BAG LARGE",			title2:"SIMON MILLER",				price: "350,000원"},
+		{title1:"Rainbow Sequin Pouch",		title2:"Attico",					price:"220,000원"},
+		{title1:"Golden Clutch",			title2:"Golden Goose Deluxe Brand",	price:"90,000원"},
+		{title1:"Bonsai Bag Mango",			title2:"Simon Miller",				price:"230,000원"},
+		{title1:"Leather Lunchbag Clutch",	title2:"Simon Miller",				price:"160,000원"},
+		{title1:"Gaia's Ark Jade",			title2:"Cult Gaia",					price:"210,000원"},
+		{title1:"Gaia's Ark Tortoise",		title2:"Cult Gaia",					price:"210,000원"},
+		{title1:"Bond Bracelet",			title2:"Charlotte Chesnais",		price:"370,000원"},
+		{title1:"Caracol Earcuff",			title2:"Charlotte Chesnais",		price:"190,000원"},
+/*10*/	{title1:"Gaia's Hoops Large",		title2:"Cult Gaia",					price:"275,000원"},
+		{title1:"Wool Socks Camel",			title2:"CAP Studio",				price:"115,000원"},
+		{title1:"Bond Bracelet",			title2:"Charlotte Chesnais",		price:"250,000원"},
+		{title1:"Caracol Earcuff",			title2:"Cult Gaia",					price:"140,000원"} ];
+
+	var accInfo=[
+		{title1:"Leather Lunchbag Clutch",	title2:"Simon Miller",				price:"110,000원"},
+		{title1:"Gaia's Ark Jade",			title2:"Cult Gaia",					price:"210,000원"},
+		{title1:"Gaia's Ark Tortoise",		title2:"Cult Gaia",					price:"210,000원"},
+		{title1:"Gaia's Ark Jade",			title2:"Cult Gaia",					price:"150,000원"},
+		{title1:"Gaia's Ark Tortoise",		title2:"Golden Goose Deluxe",		price:"90,000원"},
+		{title1:"BONSAI BAG LARGE",			title2:"SIMON MILLER",				price: "350,000원"},
+		{title1:"Rainbow Sequin Pouch",		title2:"Attico",					price:"220,000원"},
+		{title1:"Golden Clutch",			title2:"Golden Goose Deluxe Brand",	price:"90,000원"},
+		{title1:"Bonsai Bag Mango",			title2:"Simon Miller",				price:"230,000원"},
+		{title1:"Bond Bracelet",			title2:"Charlotte Chesnais",		price:"370,000원"},
+		{title1:"Caracol Earcuff",			title2:"Charlotte Chesnais",		price:"190,000원"},
+/*10*/	{title1:"Gaia's Hoops Large",		title2:"Cult Gaia",					price:"75,000원"},
+		{title1:"Wool Socks Camel",			title2:"CAP Studio",				price:"15,000원"},
+		{title1:"Logo Socks",				title2:"Paco Rabanne",				price:"18,000원"},
+		{title1:"Bonsai Bag Mango",			title2:"Simon Miller",				price:"230,000원"},
+		{title1:"Leather Clutch",			title2:"Simon Miller",				price:"110,000원"},
+		{title1:"Bond Bracelet",			title2:"Charlotte Chesnais",		price:"250,000원"},
+		{title1:"Caracol Earcuff",			title2:"Cult Gaia",					price:"140,000원"} ];
+
+	var shoesInfo=[
+		{title1:"Wool Socks Camel",			title2:"CAP Studio",				price:"150,000원"},
+		{title1:"Logo Socks",				title2:"Paco Rabanne",				price:"90,000원"},
+		{title1:"Bonsai Bag Mango",			title2:"Simon Miller",				price:"130,000원"},
+		{title1:"Gaia's Ark Jade",			title2:"Cult Gaia",					price:"150,000원"},
+		{title1:"Gaia's Ark Tortoise",		title2:"Golden Goose Deluxe",		price:"90,000원"},
+		{title1:"Leather Clutch",			title2:"Simon Miller",				price:"110,000원"},
+		{title1:"BONSAI BAG LARGE",			title2:"SIMON MILLER",				price: "150,000원"},
+		{title1:"Rainbow Sequin Pouch",		title2:"Attico",					price:"220,000원"},
+		{title1:"Golden Clutch",			title2:"Golden Goose Deluxe Brand",	price:"90,000원"},
+		{title1:"Bonsai Bag Mango",			title2:"Simon Miller",				price:"230,000원"},
+		{title1:"Leather Lunchbag Clutch",	title2:"Simon Miller",				price:"110,000원"},
+		{title1:"Gaia's Ark Jade",			title2:"Cult Gaia",					price:"210,000원"},
+		{title1:"Gaia's Ark Tortoise",		title2:"Cult Gaia",					price:"190,000원"},
+		{title1:"Bond Bracelet",			title2:"Charlotte Chesnais",		price:"270,000원"},
+		{title1:"Caracol Earcuff",			title2:"Charlotte Chesnais",		price:"190,000원"},
+/*10*/	{title1:"Gaia's Hoops Large",		title2:"Cult Gaia",					price:"75,000원"},
+		{title1:"Bond Bracelet",			title2:"Charlotte Chesnais",		price:"250,000원"},
+		{title1:"Caracol Earcuff",			title2:"Cult Gaia",					price:"140,000원"} ];
+
+
+	var aStyleItem=localStorage.aStyleItem;		// 선택한 카테고리
+	var itemInfo=undefined;						// 선택한 카테고리의 상품 정보
+
+	if(aStyleItem=="outer"){
+		itemInfo=outerInfo;
+	}else if(aStyleItem=="top"){
+		itemInfo=topInfo;
+	}else if(aStyleItem=="bottom"){
+		itemInfo=bottomInfo;
+	}else if(aStyleItem=="acc"){
+		itemInfo=accInfo;
+	}else if(aStyleItem=="shoes"){
+		itemInfo=shoesInfo;
+	}
